d2db03191816d147494fb0d9e5bbf5523a7c88e9
Vivek Sundaray <viveksundaray123@outlook.com>
2021-12-14 19:52:00 +0530

    Simplified dmenu command and added passmenu command

--- config.h
+++ config.h
@@ -65,12 +65,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-p", "Run:", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "Run:", NULL };
+static const char *passmenu[] = { "passmenu", "-p", "Pass:", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *pcmanfm[] = { "pcmanfm", "/data/vivek/", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = passmenu } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
