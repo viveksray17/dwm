db5ceb958f84277d309940ef207c43444d914ac3
Vivek Sundaray <viveksundaray123@outlook.com>
2022-01-10 07:52:06 +0530

    change name of scripts

--- config.h
+++ config.h
@@ -113,8 +113,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY,                       XK_r,      quit,           {0} },
     { MODKEY,              	        XK_n,       spawn,          SHCMD("st -e nmtui") },
-    { MODKEY,         		        XK_x,	    spawn,	    SHCMD("poweroff") },
-    { MODKEY,         		        XK_s,	    spawn,	    SHCMD("reboot") },
+    { MODKEY,         		        XK_x,	    spawn,	    SHCMD("dmenupoweroff") },
+    { MODKEY,         		        XK_s,	    spawn,	    SHCMD("dmenureboot") },
     { MODKEY|ShiftMask,		        XK_p,	    spawn,	    SHCMD("st -e add-pass") },
     { MODKEY|ShiftMask, 		XK_l,	    spawn,	    SHCMD("slock") },
     { MODKEY,         		        XK_v,	    spawn,	    SHCMD("st -e pulsemixer") },
