21882a1ab0fb5224f138d79a1ecee96d40fd5a4f
Vivek Sundaray <viveksundaray123@outlook.com>
2022-02-04 23:06:13 +0530

    added keybind for tmux-session

--- config.h
+++ config.h
@@ -113,6 +113,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY,                       XK_r,      quit,           {0} },
     { MODKEY,              	        XK_n,       spawn,          SHCMD("st -e nmtui-connect;pkill -RTMIN+15 dwmblocks") },
+    { MODKEY,              	        XK_g,       spawn,          SHCMD("tmux-sessions") },
     { MODKEY,         		        XK_x,	    spawn,	    SHCMD("dmenupoweroff") },
     { MODKEY,         		        XK_s,	    spawn,	    SHCMD("dmenureboot") },
     { MODKEY|ShiftMask,		        XK_p,	    spawn,	    SHCMD("st -e add-pass") },
